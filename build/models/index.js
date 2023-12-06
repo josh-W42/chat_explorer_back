05afa73c676875aa42fb1d3ebcc2aa4e64a3d772
Josh W <wilson.jd022@gmail.com>
2023-12-06 12:07:01 -0800

    Updating gitignore to igrnore files generated for building but keep the build directory

--- build/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ModelCreationFunctions = void 0;
-const message_1 = require("./message");
-exports.ModelCreationFunctions = {
-    Message: message_1.CreateMessageModel,
-};
